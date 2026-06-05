92572d95ad93013f9967587b6ec0c78e06c4c1f2
VT-98 <ojho10090@gmail.com>
2026-06-05 17:49:12 +0900

    refactor

--- Lec_0604/src/com/thein/proj5/Ex5_MyMain5.java
+++ Lec_0604/src/com/thein/proj5/Ex5_MyMain5.java
@@ -6,9 +6,11 @@ public class Ex5_MyMain5 {
 		
 		// 실습 진행: 간편결제 시스템
 		
+		Payment p0 = new Payment();
 		Payment p1 = new CardPayment();
-		TossPay p2 = new TossPay();
+		Payment p2 = new TossPay();
 		
+		p0.pay(999);
 		p1.pay(10000);
 		p2.pay(20000);
 	}
