ed8fe3e24367c57a641869b3fcb2bc631520a566
VT-98 <ojho10090@gmail.com>
2026-06-05 16:18:24 +0900

    add Java Errors
    자바 에러 예시

---
+++ Lec_0605/src/com/thein/proj4/Ex4.java
@@ -0,0 +1,44 @@
+package com.thein.proj4;
+
+public class Ex4 {
+
+	public static void main(String[] args) {
+		
+//		== Error ==
+		
+//		1. 세미콜론 누락
+		// int x = 5
+		// System.out.println(x);
+		
+//		2. 선언되지 않는 변수
+		// System.out.println(myVar);
+		
+//		3. 유형 불일치
+		// int x = "Hello";
+		
+//		4. 런타임 오류
+		
+//		4-1) 0으로 나누기
+	 /* int x = 10;
+		int y = 0;
+		int result = x / y;
+		System.out.println(result); */
+		
+//		4-2) 배열 인덱스 범위를 벗어남
+	 /* int[] numbers = {1, 2, 3};
+		System.out.println(numbers[8]); */
+		
+//		5. 논리적 오류
+	 /* int x = 10;
+		int y = 2;
+		int sum = x - y;
+		System.out.println("x + y = " + sum); */
+		
+		
+		
+		
+		
+
+	}
+
+}
